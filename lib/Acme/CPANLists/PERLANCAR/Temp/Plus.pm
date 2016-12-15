b82c39f33d8809f1e7e97a1bb3a16a4e34397b07
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-12-15 22:55:59 +0700

    Initial commit

---
+++ lib/Acme/CPANLists/PERLANCAR/Temp/Plus.pm
@@ -0,0 +1,17 @@
+package Acme::CPANLists::PERLANCAR::Temp::Plus;
+
+# DATE
+# VERSION
+
+our @Module_Lists = (
+    {
+        summary => "Modules I plus while MetaCPAN's ++ is not working",
+        entries => [
+            {module => 'App::Trrr', date=>'2016-12-15'},
+            {module => 'App::squaretag', date=>'2016-12-15'},
+        ],
+    },
+);
+
+1;
+# ABSTRACT:
